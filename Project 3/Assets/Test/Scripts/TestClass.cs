dc65c51a3fa90c7d58d8fe986e788f7017d06cd1
MegaDomo <joshua.villemain@gmail.com>
2023-04-04 16:44:34 -0400

    small push

--- Project 3/Assets/Test/Scripts/TestClass.cs
+++ Project 3/Assets/Test/Scripts/TestClass.cs
@@ -1,10 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TestClass : MonoBehaviour
 {
+    public event EventHandler OnPressSpaceBar;
+
     private void Start()
     {
+        OnPressSpaceBar += TestMethod;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OnPressSpaceBar?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    private void TestMethod(object sender, EventArgs e)
+    {
+        Debug.Log("Hell0");
     }
 }
