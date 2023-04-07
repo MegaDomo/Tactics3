060ad6e8cb4808caadea1a3a4bd3e2f50e238452
MegaDomo <joshua.villemain@gmail.com>
2023-04-07 16:42:03 -0400

    push

--- Project 3/Assets/UnitAssets/UI/Scripts/UnitHealth.cs
+++ Project 3/Assets/UnitAssets/UI/Scripts/UnitHealth.cs
@@ -1,22 +1,27 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UnitHealth : MonoBehaviour
 {
     [Header("Unity References")]
     public Player player;
+    public Image portraitImage;
     public Image fillImage;
+    public TextMeshProUGUI healthText;
 
     private void Start()
     {
         ChangeHealthBar(player.stats.curHealth, player.stats.maxHealth);
+        portraitImage.sprite = player.portrait;
         player.healthChangeEvent.AddListener(ChangeHealthBar);
     }
 
     public void ChangeHealthBar(int curHealth, int maxHealth)
     {
-        float cur = (float)curHealth;
-        float max = (float)maxHealth;
+        float cur = curHealth;
+        float max = maxHealth;
         fillImage.fillAmount = cur / max;
+        healthText.text = cur.ToString() + " / " + max.ToString();
     }
 }
